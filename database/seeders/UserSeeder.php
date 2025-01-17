8791f67ddd7687d735547a40ecc649d7eaa7c5e3
Kevin <keinstah@gmail.com>
2025-01-17 23:22:47 +0800

    Add impersonate

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'test@a.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
